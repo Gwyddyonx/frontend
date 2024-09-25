284d5ccc3e65faea78542307969ab69fb676330e
Gwyddyon <jkmlopez23@gmail.com>
2024-09-24 23:43:33 -0500

    building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ActualizarComponent } from './actualizar/actualizar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ActualizarComponent
   ],
   imports: [
     BrowserModule,
